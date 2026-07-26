b45766d4897a6febcb57402e9bffd4f95ef2bf8f
MohitKumarCDAC <mohit12.cdac@gmail.com>
2026-07-26 14:45:47 +0530

    initial commit

---
+++ src/main/java/com/jewellery/jewelleryshop/controller/JewelleryItemController.java
@@ -0,0 +1,48 @@
+package com.jewellery.jewelleryshop.controller;
+
+
+import com.jewellery.jewelleryshop.dto.JewelleryItemDto;
+import com.jewellery.jewelleryshop.services.JewelleryItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class JewelleryItemController {
+    @Autowired
+    private JewelleryItemService jewelleryItemService;
+
+    @PostMapping
+    public JewelleryItemDto saveItem(@RequestBody JewelleryItemDto dto)
+    {
+        return jewelleryItemService.saveItem(dto);
+    }
+
+    @GetMapping("/{itemCode}")
+    public JewelleryItemDto getItemByCode(@PathVariable String itemCode)
+    {
+        return jewelleryItemService.getItemByCode(itemCode);
+    }
+
+    @GetMapping
+    public List<JewelleryItemDto> getAllItems()
+    {
+        return jewelleryItemService.getAllItems();
+    }
+
+    @PutMapping("/{itemCode}")
+    public JewelleryItemDto updateItem(@PathVariable String itemCode,@RequestBody JewelleryItemDto dto)
+    {
+        return jewelleryItemService.updateItem(itemCode,dto);
+    }
+
+    @DeleteMapping("/{itemCode}")
+    public String deleteItem(@PathVariable String itemCode)
+    {
+        jewelleryItemService.deleteItem(itemCode);
+        return "item Deleted Successfully";
+    }
+
+}
--- src/main/java/com/jewellery/jewelleryshop/services/JewelleryItemServiceImpl.java
+++ src/main/java/com/jewellery/jewelleryshop/services/JewelleryItemServiceImpl.java
@@ -7,6 +7,8 @@ import com.jewellery.jewelleryshop.repository.JewelleryItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class JewelleryItemServiceImpl implements JewelleryItemService{
 
@@ -16,6 +18,7 @@ public class JewelleryItemServiceImpl implements JewelleryItemService{
 
     @Override
     public JewelleryItemDto saveItem(JewelleryItemDto dto){
+
         JewelleryItem item=JewelleryItem.builder()
                 .itemCode(dto.getItemCode())
                 .itemName(dto.getItemName())
@@ -27,5 +30,101 @@ public class JewelleryItemServiceImpl implements JewelleryItemService{
                 .gst(dto.getGst())
                 .price(dto.getPrice())
                 .stockQuantity(dto.getStockQuantity()).build();
+
+        JewelleryItem savedItem=jewelleryItemRepository.save(item);
+
+        return JewelleryItemDto.builder()
+                .itemCode(savedItem.getItemCode())
+                .itemName(savedItem.getItemName())
+                .category(savedItem.getCategory())
+                .purity(savedItem.getPurity())
+                .grossWeight(savedItem.getGrossWeight())
+                .netWeight(savedItem.getNetWeight())
+                .makingCharge(savedItem.getMakingCharge())
+                .gst(savedItem.getGst())
+                .price(savedItem.getPrice())
+                .stockQuantity(savedItem.getStockQuantity()).build();
     }
+
+    @Override
+    public JewelleryItemDto getItemByCode(String itemCode){
+
+        JewelleryItem item=jewelleryItemRepository.findByItemCode(itemCode)
+                .orElseThrow(()->new RuntimeException("item Not Found"));
+
+        return JewelleryItemDto.builder()
+                .itemCode(item.getItemCode())
+                .itemName(item.getItemName())
+                .category(item.getCategory())
+                .purity(item.getPurity())
+                .grossWeight(item.getGrossWeight())
+                .netWeight(item.getNetWeight())
+                .makingCharge(item.getMakingCharge())
+                .gst(item.getGst())
+                .price(item.getPrice())
+                .stockQuantity(item.getStockQuantity()).build();
+
+    }
+
+    @Override
+    public List<JewelleryItemDto> getAllItems(){
+        return jewelleryItemRepository.findAll().stream().map(item->
+                JewelleryItemDto.builder()
+                        .itemCode(item.getItemCode())
+                        .itemName(item.getItemName())
+                        .category(item.getCategory())
+                        .purity(item.getPurity())
+                        .grossWeight(item.getGrossWeight())
+                        .netWeight(item.getNetWeight())
+                        .makingCharge(item.getMakingCharge())
+                        .gst(item.getGst())
+                        .price(item.getPrice())
+                        .stockQuantity(item.getStockQuantity()).build()).toList();
+    }
+
+
+    @Override
+    public JewelleryItemDto updateItem(String itemCode,JewelleryItemDto dto){
+
+        JewelleryItem item=jewelleryItemRepository.findByItemCode(itemCode)
+                .orElseThrow(()->new RuntimeException("Item Not Found"));
+
+        item.setItemName(dto.getItemName());
+        item.setCategory(dto.getCategory());
+        item.setPurity(dto.getPurity());
+        item.setGrossWeight(dto.getGrossWeight());
+        item.setNetWeight(dto.getNetWeight());
+        item.setMakingCharge(dto.getMakingCharge());
+        item.setGst(dto.getGst());
+        item.setPrice(dto.getPrice());
+        item.setStockQuantity(dto.getStockQuantity());
+
+
+        JewelleryItem updateItem=jewelleryItemRepository.save(item);
+
+
+        return JewelleryItemDto.builder()
+                .itemCode(updateItem.getItemCode())
+                .itemName(updateItem.getItemName())
+                .category(updateItem.getCategory())
+                .purity(updateItem.getPurity())
+                .grossWeight(updateItem.getGrossWeight())
+                .netWeight(updateItem.getNetWeight())
+                .makingCharge(updateItem.getMakingCharge())
+                .gst(updateItem.getGst())
+                .price(updateItem.getPrice())
+                .stockQuantity(updateItem.getStockQuantity()).build();
+    }
+
+    @Override
+    public void deleteItem(String itemCode)
+    {
+        JewelleryItem item=jewelleryItemRepository.findByItemCode(itemCode)
+                .orElseThrow(()->new RuntimeException("item not Found"));
+
+        jewelleryItemRepository.delete(item);
+    }
+
+
+
 }
