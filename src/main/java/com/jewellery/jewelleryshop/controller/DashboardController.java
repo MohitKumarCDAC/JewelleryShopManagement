49db966b3879fc866fe8e51ed666ebbd28a0f877
MohitKumarCDAC <mohit12.cdac@gmail.com>
2026-07-30 13:08:05 +0530

    dashboard controller done

--- src/main/java/com/jewellery/jewelleryshop/controller/DashboardController.java
+++ src/main/java/com/jewellery/jewelleryshop/controller/DashboardController.java
@@ -1,4 +1,22 @@
 package com.jewellery.jewelleryshop.controller;
 
+
+import com.jewellery.jewelleryshop.dto.DashboardDto;
+import com.jewellery.jewelleryshop.services.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public DashboardDto getDashboard()
+    {
+        return dashboardService.getDashboardData();
+    }
 }
